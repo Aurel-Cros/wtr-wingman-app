1d1d1095288b7cf63cb9e9c7091c68a8835247fb
Aurélien Cros <aure.cros@gmail.com>
2023-06-22 10:50:27 +0200

    dev/feat
    Added ground functions for WebSocket class

--- src/netcode/WebSocket.js
+++ src/netcode/WebSocket.js
@@ -1,19 +1,46 @@
-class WebSocketManager {
+import WebSocket from 'ws'
+
+export default class WebSocketManager {
     constructor() {
         if (WebSocketManager.exists)
             return WebSocketManager.instance;
 
         WebSocketManager.exists = true;
         WebSocketManager.instance = this;
+
+        this.handshake() && this.initHandles();
     }
 
     handshake() {
         try {
-            const serverAddress = 'ws://localhost:8080';
+            const serverAddress = 'wss://localhost:8080';
             this._link = new WebSocket(serverAddress, 'receiver');
+            return true
         }
-        catch {
-            console.log("Connection failure.");
+        catch (err) {
+            console.log("Connection failure.", err);
         }
+    }
+
+    initHandles() {
+        this._link.on('open', () => {
+            this._link.send('Coucou serveur !');
+        })
+
+        this._link.on('message', (data) => {
+            console.log("Oh, j'ai reçu", data);
+        })
+
+        this._link.on('close', () => {
+            console.log('Logging out');
+        })
+    }
+
+    on(event, callback) {
+        this._link.on(event, callback)
+    }
+
+    send(myString) {
+        this._link.send(myString);
     }
 }
