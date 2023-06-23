e09e377644ca1aa4f2bfd6cddb88c24bca0d5947
Aurélien Cros <aure.cros@gmail.com>
2023-06-23 12:04:14 +0200

    dev/render : css
    Corrected webpack config to import assets

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,12 +1,17 @@
 const rules = require('./webpack.rules');
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-},
+rules.push(
+  {
+    test: /\.css$i/,
+    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  },
   {
     test: /\.s[ac]ss$/i,
-    use: ['css-loader', 'sass-loader']
+    use: ['style-loader', 'css-loader', 'sass-loader']
+  },
+  {
+    test: /\.(svg|jpg|png|webp)$/i,
+    type: 'asset/resource'
   });
 
 module.exports = {
