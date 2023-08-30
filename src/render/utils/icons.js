9b4765b45d34b316713cdf04b5be33aeee12bf0a
Aurélien Cros <aure.cros@gmail.com>
2023-08-30 13:02:15 +0200

    fix/dev : Icons
    Matched icons to possible weather states

---
+++ src/render/utils/icons.js
@@ -0,0 +1,18 @@
+const path = "../assets/icons/"
+import sunny from path + "sunny.svg"
+import drizzle from path + "drizzle.svg"
+import lightRain from path + "light-rain.svg"
+import mediumRain from path + "medium-rain.svg"
+import heavyRain from path + "heavy-rain.svg"
+import thunderstorm from path + "thunderstorm.svg"
+
+export default icons = {
+    weather: {
+        sunny: sunny,
+        drizzle: drizzle,
+        lightRain: lightRain,
+        mediumRain: mediumRain,
+        heavyRain: heavyRain,
+        thunderstorm: thunderstorm
+    }
+}
