ac178cd21c1a9ec74b1abeb451477d3a41a30b0c
Aurélien Cros <aure.cros@gmail.com>
2023-10-13 11:13:10 +0200

    fix/redux : Initial call
    Moved App call into renderer file directly

--- src/render/app.jsx
+++ src/render/app.jsx
@@ -1,42 +1,37 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-import Weather from './pages/Weather';
-import Telemetry from './pages/Telemetry';
-import Strategy from './pages/Strategy';
+import Weather from "./pages/Weather";
+import Telemetry from "./pages/Telemetry";
+import Strategy from "./pages/Strategy";
 
-import NavBar from './components/NavBar';
-import SideBar from './components/SideBar/';
-import InfoBar from './components/InfoBar';
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import InfoBar from "./components/InfoBar";
 
-import store from './store/store';
-import { Provider } from 'react-redux';
-import { DataProvider } from './utils/context';
+import store from "./store/store";
+import {Provider} from "react-redux";
+import {DataProvider} from "./utils/context";
 
-const domRoot = document.querySelector("#root");
-const root = createRoot(domRoot);
-
-function App() {
-    return (
-        <Provider store={store}>
-            <DataProvider>
-                <Router>
-                    <NavBar />
-                    <SideBar />
-                    <InfoBar />
-                    <main>
-                        <Routes>
-                            <Route path="/main_window" element={<Weather />} />
-                            <Route path="/weather" element={<Weather />} />
-                            <Route path="/strategy" element={<Strategy />} />
-                            <Route path="/telemetry" element={<Telemetry />} />
-                        </Routes>
-                    </main>
-                </Router>
-            </DataProvider>
-        </Provider>
-    );
+export default function App() {
+	return (
+		<Provider store={store}>
+			<DataProvider>
+				<Router>
+					<NavBar />
+					<SideBar />
+					<InfoBar />
+					<main>
+						<Routes>
+							<Route path="/main_window" element={<Weather />} />
+							<Route path="/weather" element={<Weather />} />
+							<Route path="/strategy" element={<Strategy />} />
+							<Route path="/telemetry" element={<Telemetry />} />
+						</Routes>
+					</main>
+				</Router>
+			</DataProvider>
+		</Provider>
+	);
 }
 
-root.render(App());
--- src/renderer.js
+++ src/renderer.js
@@ -5,10 +5,16 @@
  *
  * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
  */
-import './render/styles/element.scss';
-import './render/styles/layout.scss';
-import './render/styles/text.scss';
-import './render/styles/background.scss';
+import "./render/styles/element.scss";
+import "./render/styles/layout.scss";
+import "./render/styles/text.scss";
+import "./render/styles/background.scss";
 
-import './common/util';
-import './render/app';
+import "./common/util";
+
+import {createRoot} from "react-dom/client";
+import App from "./render/App";
+
+const domRoot = document.querySelector("#root");
+const root = createRoot(domRoot);
+root.render(<App />);
