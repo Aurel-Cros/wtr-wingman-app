4b4c220af7e591edb2f2b124c329116798652e41
Aurélien Cros <aure.cros@gmail.com>
2023-10-17 16:32:17 +0200

    feat/redux : Strategy
    Hooked up fuel component

--- src/render/pages/Strategy/Fuel.jsx
+++ src/render/pages/Strategy/Fuel.jsx
@@ -1,29 +1,35 @@
-import Lines from '../../components/Data/Lines';
+import { useSelector } from "react-redux";
+import Lines from "../../components/Data/Lines";
 
 export default function Fuel() {
-    return (
-        <div className="box data-box layout-col outer">
-            <h2>Fuel</h2>
-            <div className="box data-box ">
-                <Lines data={[
-                    {
-                        label: "Fuel used this stint",
-                        value: "21L"
-                    },
-                    {
-                        label: "Fuel remaining",
-                        value: "99L"
-                    },
-                    {
-                        label: "Fuel per lap",
-                        value: "3.4L"
-                    },
-                    {
-                        label: "Laps remaining",
-                        value: "27"
-                    }
-                ]} />
-            </div>
-        </div>
-    )
+	const fuel = useSelector(state => state.strategy.fuel);
+	const lapsLeft = Math.floor(fuel.remaining / fuel.perLap) || 0;
+
+	return (
+		<div className="box data-box layout-col outer">
+			<h2>Fuel</h2>
+			<div className="box data-box ">
+				<Lines
+					data={[
+						{
+							label: "Fuel used this stint",
+							value: fuel.usedThisStint,
+						},
+						{
+							label: "Fuel remaining",
+							value: fuel.remaining,
+						},
+						{
+							label: "Fuel per lap",
+							value: fuel.perLap,
+						},
+						{
+							label: "Laps remaining",
+							value: lapsLeft,
+						},
+					]}
+				/>
+			</div>
+		</div>
+	);
 }
