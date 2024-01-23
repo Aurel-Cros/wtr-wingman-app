9f8f456efbe1de1632f6d135b25ee3b28623e1f5
LeFreaky <100714646+LeFreaky@users.noreply.github.com>
2024-01-23 18:37:50 +0100

    feat/WebSocket : Server
    Added server-side teaming, handlers for a client setting their name, team name or leaving a team

--- server/index.mjs
+++ server/index.mjs
@@ -1,21 +1,59 @@
-import WebSocket from 'ws'
+import WebSocket from 'ws';
+import { nanoid } from 'nanoid';
 
+const version = "^3.0.0";
 const server = new WebSocket.Server({ port: 8080 });
 const clients = server.clients;
+const teams = new Map();
 
 server.on('connection', (client) => {
-    client.id = clients.size - 1;
-    console.log("New client", client.id);
+    client.id = nanoid();
+    console.log("New client : ", client.id);
+    client.send(JSON.stringify({
+        type: "version",
+        value: version
+    }));
 
-    console.log(clients.has(client))
+    client.on('message', (stringData) => {
+        const data = JSON.parse(stringData);
+        console.log(data);
 
-    client.send("Coucou client :)");
+        switch (data.type) {
+            case 'username':
+                client.name = data.value;
+                break;
 
-    client.on('message', data => {
-        console.log(`Oh, le client ${client.id} m'a envoyé ${data}`)
-        clients.forEach(aClient => {
-            aClient.send(`${client.id} has sent ${data}`)
-        })
+            case 'user_team':
+                if (!teams.has(data.value)) {
+                    console.log("Creating team", data.value)
+                    teams.set(data.value, new Set());
+                }
+
+                const team = teams.get(data.value);
+                team.add(client);
+                client.team = {
+                    name: data.value,
+                    users: team
+                };
+                break;
+
+            case 'leave_team':
+                client.team.users.delete(client);
+                if (client.team.users.size < 1)
+                    teams.delete(client.team.name);
+                client.team = null;
+                break;
+
+            case 'data':
+                clients.forEach(aClient => {
+                    aClient.send(stringData);
+                })
+                break;
+        }
+    })
+
+    client.on('close', () => {
+        console.log("Client closed socket.");
     })
 })
 
